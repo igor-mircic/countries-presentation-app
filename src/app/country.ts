57c723060fdb2281d08f76c551bd0f72f44dd54d
igor-mircic <igor.mircic@live.com>
2021-07-01 16:48:11 +0200

    feat(api): add interface for country

---
+++ src/app/country.ts
@@ -0,0 +1,13 @@
+export interface Country {
+  name: string;
+  nativeName: string;
+  population: number;
+  region: string;
+  subRegion: string;
+  capital: string;
+  flag: string;
+  topLevelDomain: string;
+  currencies: string;
+  languages: string;
+  borderingCountries: string;
+}
